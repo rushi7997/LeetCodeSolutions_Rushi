6d7a06f67db5379421ab380ce8d6536414f797bd
Rushi Rami <ramirushi7997@gmail.com>
2021-02-03 16:18:05 -0500

    solution 1720

---
+++ Solution_1720.java
@@ -0,0 +1,11 @@
+package com.company.Solutions;
+
+public class Solution_1720 {
+    public int[] decode(int[] encoded, int first) {
+        int[] ans = new int[encoded.length + 1];
+        ans[0] = first;
+        for (int i = 0; i < encoded.length; i ++)
+            ans[i+1] = ans[i] ^ encoded[i];
+        return ans;
+    }
+}
